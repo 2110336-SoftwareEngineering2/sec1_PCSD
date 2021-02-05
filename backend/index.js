c30029ceffea7e81f5696e90caa61ef3780f8afe
siwakorn21 <siwakornboonmun@gmail.com>
2021-02-06 05:28:37 +0700

    add auth route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const UserRoute = require('./routes/User');
+const AuthRoute = require('./routes/Authentication');
 
 const port = process.env.PORT || 4000;
 
@@ -10,6 +11,9 @@ app.use(express.json());
 
 // User route
 app.use('/user', UserRoute);
+
+// Authenticate route
+app.use('/auth', AuthRoute);
 
 app.listen(port, (error) => {
     console.log(`Listening on port ${port}`);
