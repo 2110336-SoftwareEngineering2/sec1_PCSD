2136e94863d0e13e2dd94fab62b30b8e407c5325
siwakorn21 <siwakornboonmun@gmail.com>
2021-02-06 03:03:43 +0700

    add mock up User

---
+++ backend/models/User/mock_user.js
@@ -0,0 +1,30 @@
+users_data = [
+    {
+        id: 1,
+        fistname: 'Maxwell',
+        lastname: 'Josh',
+        street: 'Wellington 1234',
+        province: 'Texas',
+        zipcode: '11111',
+        email: 'maxwelljosh@gmail.com',
+        password: 'passwordtest',
+        birthdate: '12-12-1970',
+        phone: '08123124'
+    },
+    {
+        id: 2,
+        fistname: 'Emily',
+        lastname: 'Clinton',
+        street: 'ABCDER 1234',
+        province: 'California',
+        zipcode: '20123',
+        email: 'emily1234@gmail.com',
+        password: 'passwordemily',
+        birthdate: '02-09-1982',
+        phone: '07281491'
+    },    
+]
+
+module.exports = {
+    users: users_data
+};
