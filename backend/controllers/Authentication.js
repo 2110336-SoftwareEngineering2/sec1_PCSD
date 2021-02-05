1a4d838070f63da8a14f3417d8a113124e4fe793
siwakorn21 <siwakornboonmun@gmail.com>
2021-02-06 05:30:21 +0700

    create Authentication controller

--- backend/controllers/Authentication.js
+++ backend/controllers/Authentication.js
@@ -1,11 +1,31 @@
-const User = require('../models/User/mock_user').users;
+const Users = require('../models/User/mock_user').users;
+const UserController = require('./User');
+
+// Please use hash password
+const validEmailAndPassword = (email, password) => {
+    const user = UserController.findUserByEmail(email);
+    if (user == null) {
+        return false;
+    } else {
+        if (user.password == password) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 
 module.exports = {
+    // test
     login: (req, res) => {
-        return;
+        const email = req.body.email;
+        const password = req.body.password;
+        const val = validEmailAndPassword(email, password);
+        res.send(val);
     },
 
+    // test
     logout: (req, res) => {
-        return;
+        res.send("logout");
     },
 }
