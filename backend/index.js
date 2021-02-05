29ea4d0285acef86fb8b0d219638afb906663255
siwakorn21 <siwakornboonmun@gmail.com>
2021-02-06 03:03:17 +0700

    delete test function

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+const UserRoute = require('./routes/User');
+
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send('Hello express');
-});
+// Body Parser Json
+app.use(express.json());
+
+// User route
+app.use('/user', UserRoute);
 
 app.listen(port, (error) => {
     console.log(`Listening on port ${port}`);
