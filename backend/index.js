a0741985059cef504c9f2d4bddbb72e3e7ca9a3f
Perth <perth.ploy@gmail.com>
2021-02-06 18:13:19 +0700

    Remove unused code

--- backend/index.js
+++ backend/index.js
@@ -37,12 +37,3 @@ app.listen(port, (error) => {
 app.get("/", (req, res) => {
   res.send("hello there");
 });
-
-const connectionURL =
-  "mongodb+srv://normal-user:normalguy@cluster0.e28du.mongodb.net/Caretaker?retryWrites=true&w=majority";
-
-mongoose
-  .connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log(`MongoDB connected`);
-  });
