eff32e9dd706b9524536af6b2147f5310234bf29
siwakorn21 <siwakornboonmun@gmail.com>
2021-02-07 01:29:40 +0700

    add new route to validate token

--- backend/routes/Authentication.js
+++ backend/routes/Authentication.js
@@ -2,12 +2,19 @@ const express = require('express');
 const router = express.Router();
 const AuthenController = require('../controllers/Authentication');
 
+// Login User
 router.post('/login', (req, res) => {
     AuthenController.login(req, res);
 });
 
+// Logut User
 router.post('/logout', (req, res) => {
     AuthenController.logout(req, res);
+});
+
+// Validate Token
+router.post('/valid', (req, res) => {
+    AuthenController.valid(req, res);
 });
 
 module.exports = router;
