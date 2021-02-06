227cf00cf18751904a720b4a5b26d3e6f658f3f2
siwakorn21 <siwakornboonmun@gmail.com>
2021-02-06 17:44:05 +0700

    connect MONGO DB

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 const UserRoute = require('./routes/User');
 const AuthRoute = require('./routes/Authentication');
 
-const port = process.env.PORT || 4000;
+// ENVIRONMENT VARIABLE (in .env file)
+require('dotenv').config();
+
+const port = process.env.PORT;
+const url = process.env.MONGO_URL;
+
+// Database connection
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+    .then( () => console.log('Database Connection Successfully'))
+    .catch( (err) => console.log(err));
 
 // Body Parser Json
 app.use(express.json());
@@ -15,6 +24,7 @@ app.use('/user', UserRoute);
 // Authenticate route
 app.use('/auth', AuthRoute);
 
+// Server listenining on Port 
 app.listen(port, (error) => {
     console.log(`Listening on port ${port}`);
 });
