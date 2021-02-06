e35765597f866220510b10dad6956dc9517453fa
Perth <perth.ploy@gmail.com>
2021-02-06 18:00:00 +0700

    Add connection to mongodb via mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
 
-const UserRoute = require('./routes/User');
-const AuthRoute = require('./routes/Authentication');
+const UserRoute = require("./routes/User");
+const AuthRoute = require("./routes/Authentication");
+
+const app = express();
 
 const port = process.env.PORT || 4000;
 
@@ -10,11 +12,24 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 
 // User route
-app.use('/user', UserRoute);
+app.use("/user", UserRoute);
 
 // Authenticate route
-app.use('/auth', AuthRoute);
+app.use("/auth", AuthRoute);
 
 app.listen(port, (error) => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("hello there");
+});
+
+const connectionURL =
+  "mongodb+srv://normal-user:normalguy@cluster0.e28du.mongodb.net/Caretaker?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`MongoDB connected`);
+  });
