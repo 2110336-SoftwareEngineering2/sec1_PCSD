ca6460cd37e63aed6f8963bb128ea92035c6319b
Nathavee Mahitti <apiwat-new@hotmail.co.th>
2021-02-10 23:42:33 +0700

    communication_2.0

--- backend/communication/CommuController.js
+++ backend/communication/CommuController.js
@@ -1,3 +1,3 @@
 const ChatBox = require('./ChatBox');
 
-ChatBox.sent('6023f9e642f6314e3489f03a',"ควย",'6023e7e3a9eef55d009415a6');
+ChatBox.sent('6023f9e642f6314e3489f03a',"hey",'6023e7e3a9eef55d009415a6');
