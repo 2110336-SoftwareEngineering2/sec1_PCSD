96437c5c7e431abd6334c558b7950d101e6fb9d0
Natnaree Panya <umimchoi@gmail.com>
2021-02-16 01:04:24 +0700

    Login & Register Page(html&css)

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,24 +1,41 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    console.log('render email', email);
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container my-4">
+      <div className="row">
+      <div className="col12 col-md-8 offset-md-2">
+    <form>
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" 
+      className="form-control" 
+      id="exampleInputEmail1" 
+      aria-describedby="emailHelp" 
+      onchange={(e) => setEmail(e.target.value)} />
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label for="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" 
+    onchange={(e) => setPass(e.target.value)}
+/>
+  </div>
+  <div className="mb-3 form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" for="exampleCheck1">Remember Me</label>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+      </div>
+      </div>
+      </div>
   );
 }
 
