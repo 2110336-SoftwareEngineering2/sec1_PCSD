81fe155d0eb80e7b87cb53db0cea936134fcb3a8
Perth <perth.ploy@gmail.com>
2021-02-16 19:18:15 +0700

    Implement pet removal system

--- backend/controllers/Pet.js
+++ backend/controllers/Pet.js
@@ -4,17 +4,46 @@ const Pet = require("../models/User/Pet-model");
 
 module.exports = {
   addPet: (req, res) => {
+    // Check if user is logged in
     checkAuth.validateAccessToken(req, res);
+    const user = req.decoded;
 
-    if (req.decoded && req.decoded.role === "user") {
+    // user is logged in and hase "user" role
+    if (user && user.role === "user") {
       const petInfo = req.body;
-      const petOwner = req.decoded.email;
-      const newPet = new Pet({ ...petInfo, owner: petOwner.email });
+      const { email } = user.email;
+      // create new pet
+      const newPet = new Pet({ ...petInfo, owner: email });
 
       newPet.save((err) => {
         if (err) console.log(err);
         res.send(newPet);
       });
+    }
+  },
+  removePet: async (req, res) => {
+    // Check if user is logged in
+    checkAuth.validateAccessToken(req, res);
+    const user = req.decoded;
+
+    // user is logged in and has "user" role
+    if (user && user.role === "user") {
+      const petName = req.body.petName;
+      const { email } = user.email;
+      // remove pet
+      await Pet.findOneAndDelete(
+        {
+          petName: petName,
+          owner: email,
+        },
+        (err) => {
+          if (err) {
+            res.send(err);
+          } else {
+            res.send("Remove pet successful");
+          }
+        }
+      );
     }
   },
 };
