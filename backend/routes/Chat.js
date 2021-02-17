b9a71367e896a2429533714ec62c788b3a96fc03
siwakorn21 <siwakornboonmun@gmail.com>
2021-02-18 02:27:31 +0700

    update chat router

--- backend/routes/Chat.js
+++ backend/routes/Chat.js
@@ -2,16 +2,20 @@ const express = require("express");
 const router = express.Router();
 const ChatController = require('../controllers/Chatroom');
 
-router.get("/", (req, res) => {
+router.get("/rooms", (req, res) => {
     ChatController.getAllChatrooms(req, res);
 });
 
-router.post("/", (req, res) => {
+router.post("/create", (req, res) => {
     ChatController.createChatrooms(req, res);
 });
 
 router.get("/:email", (req, res) => {
     ChatController.getUserChatrooms(req, res);
 });
+
+router.get("/rooms/:id", (req, res) => {
+    ChatController.getChatRoomById(req, res);
+})
 
 module.exports = router;
