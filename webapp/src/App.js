bce2acc58f5d5bb8179966de0c23b2c73d6498f2
Perth <perth.ploy@gmail.com>
2021-02-18 08:12:12 +0700

    Create UserLogin component

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,41 +1,18 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+
+import UserLogin from "./component/login-component";
 
 function App() {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    console.log('render email', email);
-    
+  // const [email, setEmail] = useState('');
+  // const [pass, setPass] = useState('');
+  // console.log('render email', email);
+
   return (
-      <div className="container my-4">
-      <div className="row">
-      <div className="col12 col-md-8 offset-md-2">
-    <form>
-  <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" 
-      className="form-control" 
-      id="exampleInputEmail1" 
-      aria-describedby="emailHelp" 
-      onchange={(e) => setEmail(e.target.value)} />
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label for="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" 
-    onchange={(e) => setPass(e.target.value)}
-/>
-  </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-    <label className="form-check-label" for="exampleCheck1">Remember Me</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
-      </div>
-      </div>
-      </div>
+    <Router>
+      <Route path="/" exact component={UserLogin} />
+    </Router>
   );
 }
 
---
+++ webapp/src/component/login-component.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+
+export default class UserLogin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  setEmail(email) {
+    this.setState({
+      email: email,
+    });
+  }
+
+  setPass(password) {
+    this.setState({
+      password: password,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    window.location = "/login";
+  }
+
+  render() {
+    return (
+      <div className="container my-4">
+        <div className="row">
+          <div className="col12 col-md-8 offset-md-2">
+            <form onSubmit={(e) => this.onSubmit(e)}>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  onChange={(e) => this.setEmail(e.target.value)}
+                />
+                <div id="emailHelp" className="form-text">
+                  We'll never share your email with anyone else.
+                </div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  onChange={(e) => this.setPass(e.target.value)}
+                />
+              </div>
+              <div className="mb-3 form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label className="form-check-label" htmlFor="exampleCheck1">
+                  Remember Me
+                </label>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
