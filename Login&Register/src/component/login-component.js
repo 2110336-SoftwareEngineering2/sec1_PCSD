4da9a53f6b2ca95d05bc0914381230ac60cb01b9
Natnaree Panya <umimchoi@gmail.com>
2021-02-20 00:44:16 +0700

    Edit Submit Buttons

--- Login&Register/src/component/login-component.js
+++ Login&Register/src/component/login-component.js
@@ -42,53 +42,6 @@ export default class UserLogin extends Component {
 
   render() {
     return (
-      /*<div className="container my-4">
-        <div className="row">
-          <div className="col12 col-md-8 offset-md-2">
-            <form onSubmit={(e) => this.onSubmit(e)}>
-              <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  onChange={(e) => this.setEmail(e.target.value)}
-                />
-                <div id="emailHelp" className="form-text">
-                  We'll never share your email with anyone else.
-                </div>
-              </div>
-              <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                  onChange={(e) => this.setPass(e.target.value)}
-                />
-              </div>
-              <div className="mb-3 form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="exampleCheck1"
-                />
-                <label className="form-check-label" htmlFor="exampleCheck1">
-                  Remember Me
-                </label>
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
-      </div> */
     <div className="login__register">
         <ul class="tabs">
 		<li class="active">Login</li>
