9b530f1932a2cfa04f5b7cb3a4f347fb77eb08e8
Natnaree Panya <umimchoi@gmail.com>
2021-02-20 00:36:08 +0700

    Beautify Page

--- Login&Register/src/Logo.js
+++ Login&Register/src/Logo.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import "./component/style.css";
 import petidcare from "./petidcare.png";
 
 function Logo() {
