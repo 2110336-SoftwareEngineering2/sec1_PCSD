d0d40448e59832095f2d7b376b9a8315f0a77a08
Natnaree Panya <umimchoi@gmail.com>
2021-02-20 21:57:26 +0700

    add header UserInfo

--- webapp/src/Register/Petowner.js
+++ webapp/src/Register/Petowner.js
@@ -5,9 +5,9 @@ import UserProfile from "./UserProfile";
 import UserInfo from "./UserInfo";
 function Petowner() {
   return (
-    <div className="Petowner">
-        <p>This is Petowner register page</p>
-        {/*User Profile*/}
+    <div className="petowner">
+        <UserProfile />
+        <UserInfo />
         {/*User Info*/}
     </div>
 
@@ -15,4 +15,3 @@ function Petowner() {
 }
 
 export default Petowner;
-
--- webapp/src/Register/UserInfo.js
+++ webapp/src/Register/UserInfo.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./UserInfo.css";
+import Register_info from "./Register_info";
+import Register_header from "./Register_header";
 function UserInfo() {
   return (
     <div className="userInfo">
+      <Register_header />
+      <Register_info  />
+      {/*regis_header*/}
+      {/*regis_info*/}
     </div>
 
   );
