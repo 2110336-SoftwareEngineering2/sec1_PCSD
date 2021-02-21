5dd1a2035d79df4072854c3e1fd10fd50336501c
Natnaree Panya <umimchoi@gmail.com>
2021-02-21 12:40:50 +0700

    edit routes

--- webapp/src/Routes.js
+++ webapp/src/Routes.js
@@ -12,12 +12,12 @@ import history from './history';
 export default class Routes extends Component {
     render() {
         const user = null;
-        const role = "petowner";
+        const role = "caretaker";
         return (
             <Router history={history}>
                 <Switch>
                     <Route path="/" exact component={ !user ? LoginPage : Home } />
-                    <Route path="/register" exact component={ role == "petowner" ? Petowner : Caretaker  } />
+                    <Route path="/register" component={ role == "petowner" ? Petowner : Caretaker  } />
                 </Switch>
             </Router>
         );
