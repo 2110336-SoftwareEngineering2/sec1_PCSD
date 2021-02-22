9b33818b4d81b7ec6fc36ac4245f75ac1aacfb4c
Natchanon Wangpruek <lnwnut227@gmail.com>
2021-02-22 12:07:02 +0700

    push some structure there are lots of changes

--- webapp/src/Chat/chat.js
+++ webapp/src/Chat/chat.js
@@ -3,5 +3,35 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./chat.css"
 
 export default class ChatPage extends React.Component{
-    
+    render(){
+        <html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Chat Mock</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css"/>
+  </head>
+  <body>
+    <div id ="chat-container">
+      <div id = "search-container">
+        <input type="text" placeholder="Seach on Chats:"/>
+      </div>
+      <div id = "conversation-list">
+        
+      </div>
+      <div id = "new-chat-container">
+        <a href="#">+</a>
+      </div>
+      <div id = "chat-title">
+
+      </div>
+      <div id = "chat-mesages">
+
+      </div>
+      <div id = "chat-form">
+
+      </div>
+    </div>
+  </body>
+</html>
+    }
 }
