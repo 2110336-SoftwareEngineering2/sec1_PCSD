aa50ac0d6e1afa0602edd3a64211518d72ec280e
malachi3885 <malachi3885@gmail.com>
2021-02-22 16:27:45 +0700

    Add banpage

--- webapp/src/Routes.js
+++ webapp/src/Routes.js
@@ -1,33 +1,31 @@
 import React, { Component } from "react";
 import { Router, Switch, Route } from "react-router-dom";
 
-
+import BanPage from "./banpage";
 import LoginPage from "./LoginPage";
 import Home from "./Home/Home";
 import Caretaker from "./Register/Caretaker";
 import Petowner from "./Register/Petowner";
 import history from "./history";
 import ChatPage from "./Chat/chat";
+import Header from "./header";
 
-function Routes(){
-    const user = null;
-    const role = "petowner";
-    return (
-      <Router history={history}>
-        <Switch>
-          <Route path="/" exact component={!user ? LoginPage : Home} />
-          <Route
-            path="/register" 
-            component={role == "petowner" ? Petowner : Caretaker}
-          />
-          <Route
-            path="/chat" 
-            component={ChatPage}
-          />
-        </Switch>
-      </Router>
-    );
-
+function Routes() {
+  const user = null;
+  const role = "petowner";
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact component={!user ? LoginPage : Home} />
+        <Route
+          path="/register"
+          component={role == "petowner" ? Petowner : Caretaker}
+        />
+        <Route path="/chat" component={ChatPage} />
+        <Route path="/banpage" component={BanPage} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default Routes;
---
+++ webapp/src/banpage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BanTable } from "./component/BanTable";
+import "./banpage.css";
+
+function BanPage() {
+  return (
+    <div className="table">
+      <BanTable />
+    </div>
+  );
+}
+
+export default BanPage;
