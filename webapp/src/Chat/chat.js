38c15506ffdbbee51eb049c6be773f4200662d14
Natchanon Wangpruek <lnwnut227@gmail.com>
2021-02-22 12:14:25 +0700

    Edit Sth

--- webapp/src/Chat/chat.js
+++ webapp/src/Chat/chat.js
@@ -4,34 +4,25 @@ import "./chat.css"
 
 export default class ChatPage extends React.Component{
     render(){
-        <html lang="en">
-  <head>
-    <meta charset="utf-8"/>
-    <title>Chat Mock</title>
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css"/>
-  </head>
-  <body>
-    <div id ="chat-container">
-      <div id = "search-container">
-        <input type="text" placeholder="Seach on Chats:"/>
-      </div>
-      <div id = "conversation-list">
-        
-      </div>
-      <div id = "new-chat-container">
-        <a href="#">+</a>
-      </div>
-      <div id = "chat-title">
+        <div className ="chat-container">
+        <div id = "search-container">
+            <input type="text" placeholder="Seach on Chats:"/>
+        </div>
+        <div id = "conversation-list">
+            
+        </div>
+        <div id = "new-chat-container">
+            <a href="#">+</a>
+        </div>
+        <div id = "chat-title">
 
-      </div>
-      <div id = "chat-mesages">
+        </div>
+        <div id = "chat-mesages">
 
-      </div>
-      <div id = "chat-form">
+        </div>
+        <div id = "chat-form">
 
-      </div>
-    </div>
-  </body>
-</html>
+        </div>
+        </div>
     }
 }
