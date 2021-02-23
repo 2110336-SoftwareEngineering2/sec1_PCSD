9c3b77b038dad10e819816f8f43f18d245a303d1
Perth <perth.ploy@gmail.com>
2021-02-23 10:11:12 +0700

    Add ban route

--- webapp/src/Routes.js
+++ webapp/src/Routes.js
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
 import { Router, Switch, Route } from "react-router-dom";
 
-<<<<<<< HEAD
-=======
 import BanPage from "./banpage";
->>>>>>> 25fcafa2f79b66fe64c4117007e4be2d9f38167e
 import LoginPage from "./LoginPage";
 import Home from "./Home/Home";
 import Caretaker from "./Register/Caretaker";
 import Petowner from "./Register/Petowner";
 import history from "./history";
 import ChatPage from "./Chat/chat";
-<<<<<<< HEAD
 import MyContext from "./component/MyContext";
 
 function Routes() {
@@ -36,27 +32,10 @@ function Routes() {
             component={values.role == "petowner" ? Petowner : Caretaker}
           />
           <Route path="/chat" component={ChatPage} />
+          <Route path="/banpage" component={BanPage} />
         </Switch>
       </Router>
     </MyContext.Provider>
-=======
-import Header from "./Header/header";
-function Routes() {
-  const user = null;
-  const role = "petowner";
-  return (
-    <Router history={history}>
-      <Switch>
-        <Route path="/" exact component={!user ? LoginPage : Home} />
-        <Route
-          path="/register"
-          component={role == "petowner" ? Petowner : Caretaker}
-        />
-        <Route path="/chat" component={ChatPage} />
-        <Route path="/banpage" component={BanPage} />
-      </Switch>
-    </Router>
->>>>>>> 25fcafa2f79b66fe64c4117007e4be2d9f38167e
   );
 }
 
