c31fe999849135b58f826edf495763a6e3e669e0
Natnaree Panya <umimchoi@gmail.com>
2021-02-24 01:32:36 +0700

    AddPet 78% done

--- webapp/src/Register/AddButton.js
+++ webapp/src/Register/AddButton.js
@@ -10,31 +10,36 @@ function AddButton() {
     const [isNext, setIsNext] = useState(false);
     const [isAdd, setIsAdd] = useState(false);
     const [pet_lists, setPetlists] = useState([]);
+
     
     function clickedAdd(event) {
-        if(!isNext && !isAdd){
-        setIsNext(true);
-        setIsAdd(true);
-        }
-        else { setIsNext(false);
+        if(!isAdd) setIsAdd(true);
+        else {
             setIsAdd(false);
+            setIsNext(false);
         }
     }
     
-    function addPetList(name) {
-        const newPet = {id, name};
+    function addPetList(name, img, age, breed, gender) {
+        const newPet = {id, name, img, age, breed, gender};
         setPetlists([newPet, ...pet_lists]);
         id +=1;
+        setIsNext(true);
+    }
+
+    function deletePet(id) {
+        setPetlists(pet_lists.filter((pet) => pet.id !== id));
     }
     
         return( 
             <div>
-                {!isNext ? <AddPet addPetList = {addPetList} click = {isAdd} /> : <SumPet pet_lists={pet_lists} id={id}/> }
+                {!isNext ? <AddPet addPetList = {addPetList} click = {isAdd} /> : <SumPet pet_lists={pet_lists} deletePet={deletePet}/> }
                  <div className="addbutton">
                     <button className="submit" onClick={clickedAdd}>Add Pet</button>
                  </div> 
             </div> 
         );
+    
 }
 
 export default AddButton;
