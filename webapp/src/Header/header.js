f430a78f4f2d32889a801ae77e17096d71fc538a
Perth <perth.ploy@gmail.com>
2021-03-16 18:42:57 +0700

    Fix header

--- webapp/src/Header/header.js
+++ webapp/src/Header/header.js
@@ -9,6 +9,7 @@ import history from "./../history";
 import { UserContext } from "../context/MyContext";
 import { DropdownButton, MenuItem } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 function Header() {
   const { user, logout } = useContext(UserContext);
@@ -52,6 +53,7 @@ function Header() {
               </div>
             }
             id="dropdown-menu-align-right"
+            data-toggle="dropdown"
           >
             <MenuItem
               eventKey="1"
