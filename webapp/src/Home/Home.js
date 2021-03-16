02df4f2db0bd180b5a7ec5b2242d32f6e6d02a66
Natnaree Panya <umimchoi@gmail.com>
2021-03-16 21:06:15 +0700

    adding search box to home page

--- webapp/src/Home/Home.js
+++ webapp/src/Home/Home.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./../Header/header";
 import "./Home.css";
 import background from "./bg.jpg";
+import SearchBox from "./../SearchPage/SearchBox";
 function Home() {
   return (
     <div className="home" style={{ 
       backgroundImage: `url(${background})`
     }}>
         <Header />
+        <SearchBox />
     </div>
 
   );
