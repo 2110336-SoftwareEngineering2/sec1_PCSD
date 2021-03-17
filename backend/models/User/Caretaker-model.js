6b5ca2f989a459fb09dd1339ed17d2494d5a3163
Nathavee Mahitti <apiwat-new@hotmail.co.th>
2021-03-17 11:32:59 +0700

    caretaker model

---
+++ backend/models/User/Caretaker-model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+// Basic user schema
+const CaretakerSchema = new Schema({
+  email: String,
+  type : [String],
+  pet_type : [String],
+  city : String,
+  province : String,
+  country : String,
+  description : String,
+  role : String,
+  available_day : [String],
+  rate: {
+    type: mongoose.Decimal128,
+    get: v => new mongoose.Types.Decimal128((+v.toString()).toFixed(2))
+    },
+});
+
+module.exports = mongoose.model("Caretaker", CaretakerSchema, "Users");
