ba9cc1b30a55d3dce0fe6e82a8be8db91316196e
Nathavee Mahitti <apiwat-new@hotmail.com>
2021-03-18 02:10:49 +0700

    search2

--- backend/controllers/Caretaker.js
+++ backend/controllers/Caretaker.js
@@ -1,9 +1,9 @@
 const Caretaker = require("../models/User/Caretaker-model");
 
-const getCaretaker = async (req, res) => {
-  const caretakerInfo = await Caretaker.findOne({email:req.body.email});
-  res.send(caretakerInfo);
-};
+// const getCaretaker = async (req, res) => {
+//   const caretakerInfo = await Caretaker.findOne({email:req.body.email});
+//   res.send(caretakerInfo);
+// };
 
 const addCaretaker = async (req, res) => {
   const newCaretaker = new Caretaker({
