26d61cf674d1a9a0220e58fc54f02b483610ff1e
Nathavee Mahitti <apiwat-new@hotmail.co.th>
2021-03-21 14:19:46 +0700

    update search

--- backend/controllers/Caretaker.js
+++ backend/controllers/Caretaker.js
@@ -1,4 +1,5 @@
 const Caretaker = require("../models/User/Caretaker-model");
+const User = require("../models/User/User-model");
 
 const getCaretaker = async (req, res) => {
   const caretakerInfo = await Caretaker.findOne({email:req.body.email});
@@ -21,40 +22,62 @@ const updateCaretaker = async (req, res) => {
 };
 
 const SearchCaretaker = async (body,res) =>{
-    const fillter = {
-       rate : {$gte:0},
-      //  $or : [{city : "Bangkok"}]
-    };
-    if ( body.minrate ){
-      fillter.rate.$gte = body.minrate;
-    }
-    if ( body.maxrate ){
-      fillter.rate.$lte = body.maxrate;
-    }
+  const fillter = {
+     rate : {$gte:0},
+  };
+  if ( body.minrate ){
+    fillter.rate.$gte = body.minrate;
+  }
+  if ( body.maxrate ){
+    fillter.rate.$lte = body.maxrate;
+  }
 
-    if (body.type){
-      fillter.type = { $in : body.type };
-    }
+  if (body.type){
+    fillter.type = { $in : body.type };
+  }
 
-    if (body.pet_type){
-      fillter.pet_type = { $in : body.pet_type };
-    }
+  if (body.pet_type){
+    fillter.pet_type = { $in : body.pet_type };
+  }
 
-    if (body.address){
-      fillter.$or = [
-        {city:{ $regex: body.address , $options: "i"}},
-        {province:{ $regex: body.address , $options: "i"}},
-        {country:{ $regex: body.address , $options: "i"}}  ];
-    }
+  if (body.address){
+    fillter.$or = [
+      {city:{ $regex: body.address , $options: "i"}},
+      {province:{ $regex: body.address , $options: "i"}},
+      {country:{ $regex: body.address , $options: "i"}}  ];
+  }
+  // console.log("is run");
 
-    await Caretaker.find(fillter, (err, result) => {
-      if (err) {
-        res.status(400).send(fillter);
-      } else {
-        res.status(200).json(result);
-        console.log(fillter);
+  await Caretaker.find(fillter, '-_id caretaker rate city province country description', (err, result) => {
+    if (err) {
+      res.status(400).send("not found");
+    } else {
+      // res.status(200).json(result);
+      // console.log(fillter);
+      const emaillist = [];
+      const response = {};
+      for (var email of result){
+        emaillist.push(email.caretaker);
+        response[email.caretaker] = {user: "" ,caretaker : email};
       }
-    });
+      console.log(emaillist);
+
+      User.find({ email : {$in : emaillist}}, (err, finalresult) => {
+        if (err) {
+          res.status(400).send(fillter);
+        } else {
+          for (let user of finalresult){
+            response[user.email]["user"] = {
+              username: user.username,
+              firstname: user.firstname,
+              lastname: user.lastname
+            };
+          }
+          res.status(200).json(response);
+        }
+      });
+    }
+  });
 };
 module.exports = {
     addCaretaker,
