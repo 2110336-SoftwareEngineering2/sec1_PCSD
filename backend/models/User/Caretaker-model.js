07cf3d5e26588ca83c7e035949a74bbce3dc7ffd
Nathavee Mahitti <apiwat-new@hotmail.co.th>
2021-03-21 22:51:58 +0700

    review

--- backend/models/User/Caretaker-model.js
+++ backend/models/User/Caretaker-model.js
@@ -3,6 +3,27 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 // Basic user schema
+const comment = new Schema({
+  email: String,
+  text: String,
+  date: {type: Date,default: Date.now}
+});
+
+const rate_point = new Schema({
+  sum_rate : {
+    type: mongoose.Decimal128,
+    get: v => new mongoose.Types.Decimal128((+v.toString()).toFixed(2)),
+    },
+  rate_count : {type: Number},
+  raw_rate : {
+    rater : String,
+    rate : {
+      type: mongoose.Decimal128,
+      get: v => new mongoose.Types.Decimal128((+v.toString()).toFixed(2)),
+      },
+  }
+});
+
 const CaretakerSchema = new Schema({
   caretaker: String,
   type : [String],
@@ -13,10 +34,15 @@ const CaretakerSchema = new Schema({
   description : String,
   role : String,
   available_day : [String],
-  rate: {
+  rate : {
     type: mongoose.Decimal128,
-    get: v => new mongoose.Types.Decimal128((+v.toString()).toFixed(2))
+    get: v => new mongoose.Types.Decimal128((+v.toString()).toFixed(2)),
     },
+  rate_point : {type: rate_point,default:{
+    sum_rate : 0,
+    rate_count : 0,
+  }},
+  comment : [comment],
   reserved : String,
   
 });
