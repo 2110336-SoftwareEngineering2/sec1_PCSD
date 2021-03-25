54bacef77680e4e7664709096b1645e0284f5692
Ribbinnn <r.supichaya29@gmail.com>
2021-03-25 19:14:34 +0700

    reserve page with data (from test account)

--- webapp/src/ReserveCaretaker.js
+++ webapp/src/ReserveCaretaker.js
@@ -1,13 +1,84 @@
-import React, { useContext, useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import "./ReserveCaretaker.css";
 import image from "./userpic.png";
-import Header from "./Header/header"
+import Header from "./Header/header";
 import RoomOutlinedIcon from '@material-ui/icons/RoomOutlined';
 import PhoneRoundedIcon from '@material-ui/icons/PhoneRounded';
 import MailOutlinedIcon from '@material-ui/icons/MailOutlined';
+import axios from "axios";
 
 function ReserveCaretaker() {
+    const [name, setName] = useState({firstname: null, lastname: null});
+    const [contact, setContact] = useState({email: null, phone: null});
+    const [description, setDesc] = useState({desc: null});
+    const [serviceArea, setArea] = useState({area: null});
+    const [rate, setRate] = useState({rate: null});
+    const [serviceType, setServiceType] = useState({serviceType: null});
+    const [petType, setPetType] = useState({petType: null});
+    const [availDays, setAvailDays] = useState({availDays: null});
+    
+    useEffect(() => {
+        axios
+        .post("http://localhost:4000/user/email", {email: "testtttt@gmail.com"})
+        .then((res) => {
+            const data = res.data;
+            setName({firstname: data.firstname, lastname: data.lastname});
+            setContact({email: data.email, phone: data.mobileNumber});
+            })
+        .catch((err) => {
+            console.log(err);
+            });
+        
+        axios
+        .post("http://localhost:4000/user/caretaker/find", {caretaker: "testtttt@gmail.com"})
+        .then((res) => {
+            const data = res.data;
+            setDesc({desc: data.description});
+            const area = data.city + ", " + data.province + ", " + data.country;
+            setArea({area: area});
+            setRate({rate: data.rate.$numberDecimal});
+            var serviceType = "";
+            for (var i = 0; i < data.type.length; i++) {
+                var type = "";
+                if (data.type[i] == "housesitting") {
+                    type = "House Sitting";
+                } else if (data.type[i] == "boarding") {
+                    type = "Boarding";
+                } else {
+                    type = "Day Care";
+                }
+                if (i == data.type.length-1) {
+                    serviceType += type;
+                } else {
+                    serviceType += type + ", ";
+                }
+            }
+            setServiceType({serviceType: serviceType});
+            var petType = "";
+            for (var i = 0; i < data.pet_type.length; i++) {
+                const pet = data.pet_type[i].charAt(0).toUpperCase() + data.pet_type[i].slice(1);
+                if (i == data.pet_type.length-1) {
+                    petType += pet;
+                } else {
+                    petType += pet + ", ";
+                }
+            }
+            setPetType({petType: petType});
+            var availDays = "";
+            for (var i = 0; i < data.available_day.length; i++) {
+                const day = data.available_day[i].charAt(0).toUpperCase() + data.available_day[i].slice(1);
+                if (i == data.available_day.length-1) {
+                    availDays += day;
+                } else {
+                    availDays += day + ", ";
+                }
+            }
+            setAvailDays({availDays: availDays});
+            })
+        .catch((err) => {
+            console.log(err);
+            });
+    }, []);
     
     return (
         <div className="reserve">
@@ -19,28 +90,28 @@ function ReserveCaretaker() {
                             <img className="reserveimg" src={image}/>
                         </div>
                         <div className="col-9 reserve_userinfo">
-                            <label className="namelabel">Firstname &nbsp; Surname</label><br/>
-                            <label className="greylabel">description description description description description description</label>
+                            <label className="namelabel">{name.firstname} &nbsp; {name.lastname}</label><br/>
+                            <label className="greylabel">{description.desc}</label>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-5 reserve_contact">
                             <RoomOutlinedIcon fontSize="large"/>
-                            <label className="datalabel">Service Area</label><br/>
+                            <label className="datalabel">{serviceArea.area}</label><br/>
                             <PhoneRoundedIcon fontSize="large"/>
-                            <label className="datalabel">Phone Number</label><br/>
+                            <label className="datalabel">{contact.phone}</label><br/>
                             <MailOutlinedIcon fontSize="large"/>
-                            <label className="datalabel">Email</label>
+                            <label className="datalabel">{contact.email}</label>
                         </div>
                         <div className="col-7 reserve_service">
                             <label className="greylabel">Service Type:</label>
-                            <label className="datalabel">Service Type</label><br/>
+                            <label className="datalabel">{serviceType.serviceType}</label><br/>
                             <label className="greylabel">Pet Type:</label>
-                            <label className="datalabel">Pet Type</label><br/>
+                            <label className="datalabel">{petType.petType}</label><br/>
                             <label className="greylabel">Available Day(s):</label>
-                            <label className="datalabel">Days</label><br/>
+                            <label className="datalabel">{availDays.availDays}</label><br/>
                             <label className="greylabel">Rate per hour (baht):</label>
-                            <label className="datalabel">Rate</label>
+                            <label className="datalabel">{rate.rate}</label>
                         </div>
                     </div>
                     <div className="row">
