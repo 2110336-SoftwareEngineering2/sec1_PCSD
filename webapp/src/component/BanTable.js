c0ecb3ddd9a1a56abe4686914d94f78f504cdd89
malachi3885 <malachi3885@gmail.com>
2021-03-26 23:28:01 +0700

    Ban page only show 10 result per page

--- webapp/src/component/BanTable.js
+++ webapp/src/component/BanTable.js
@@ -1,5 +1,5 @@
 import React, { useMemo, useEffect, useState } from "react";
-import { useTable } from "react-table";
+import { useTable, usePagination } from "react-table";
 import MOCK_DATA from "./MOCK_DATA.json";
 import { COLUMNS } from "./columns";
 import "./BanTable.css";
@@ -8,6 +8,7 @@ import IconButton from "@material-ui/core/IconButton";
 import { Button } from "@material-ui/core";
 import CancelIcon from "@material-ui/icons/Cancel";
 import axios from "axios";
+import ReactPaginate from "react-paginate";
 
 export const BanTable = () => {
   const [data, setData] = useState([]);
@@ -23,10 +24,7 @@ export const BanTable = () => {
   const columns = useMemo(() => COLUMNS, []);
   // const data = useMemo(() => MOCK_DATA, []);
   // const data = dataFromBack;
-  const tableInstance = useTable({
-    columns,
-    data,
-  });
+
   function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
@@ -58,136 +56,254 @@ export const BanTable = () => {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    state,
+    gotoPage,
+    pageCount,
     prepareRow,
-  } = tableInstance;
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    usePagination
+  );
+
+  const { pageIndex } = state;
+
   return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup, i) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => {
-              return (
-                <th {...column.getHeaderProps()}>
-                  {column.render("Header")}
-                  {/* <hr /> */}
-                </th>
-              );
-            })}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          const status = row.values.banStatus;
-          const deleteButton = false;
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                if (cell.column.Header === "Ban/Delete" && status === true) {
-                  return (
-                    <td>
-                      <Button
-                        variant="contained"
-                        style={{
-                          padding: "0px",
-                          fontSize: "11px",
-                          color: "white",
-                          backgroundColor: "#43BD48",
-                          marginRight: "8px",
-                          marginBottom: "3px",
-                        }}
-                        onClick={() => handleBan(row.original)}
-                      >
-                        Unban
-                      </Button>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        style={{
-                          padding: "0px",
-                          fontSize: "11px",
-                          width: "5px",
-                          marginBottom: "3px",
-                        }}
-                        disabled
-                      >
-                        Ban
-                      </Button>
-                      <IconButton aria-label="delete">
-                        <CancelIcon />
-                      </IconButton>
-                      {/* <hr className="ban_line" /> */}
-                    </td>
-                  );
-                } else if (
-                  cell.column.Header === "Ban/Delete" &&
-                  status === false
-                ) {
-                  return (
-                    <td>
-                      <Button
-                        variant="contained"
-                        style={{
-                          padding: "0px",
-                          fontSize: "11px",
-                          marginRight: "8px",
-                          marginBottom: "3px",
-                        }}
-                        disabled
-                      >
-                        Unban
-                      </Button>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        style={{
-                          padding: "0px",
-                          fontSize: "11px",
-                          backgroundColor: "#C53030",
-                          width: "5px",
-                          marginBottom: "3px",
-                        }}
-                        onClick={() => handleBan(row.original)}
-                      >
-                        Ban
-                      </Button>
-                      <IconButton aria-label="delete">
-                        <CancelIcon onClick={() => deleteUser(row.original)} />
-                      </IconButton>
-                      {/* <hr className="ban_line" /> */}
-                    </td>
-                  );
-                } else {
-                  if (cell.value === true) {
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup, i) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => {
+                return (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                    {/* <hr /> */}
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            const status = row.values.banStatus;
+            const deleteButton = false;
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  if (cell.column.Header === "Ban/Delete" && status === true) {
                     return (
                       <td>
-                        <h4>Banned</h4>
-                        {/* <hr className="normal_line" /> */}
+                        <Button
+                          variant="contained"
+                          style={{
+                            padding: "0px",
+                            fontSize: "11px",
+                            color: "white",
+                            backgroundColor: "#43BD48",
+                            marginRight: "8px",
+                            marginBottom: "0px",
+                          }}
+                          onClick={() => handleBan(row.original)}
+                        >
+                          Unban
+                        </Button>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          style={{
+                            padding: "0px",
+                            fontSize: "11px",
+                            width: "5px",
+                            marginBottom: "0px",
+                          }}
+                          disabled
+                        >
+                          Ban
+                        </Button>
+                        <IconButton
+                          aria-label="delete"
+                          style={{
+                            height: "40px",
+                            width: "40px",
+                          }}
+                        >
+                          <CancelIcon />
+                        </IconButton>
+                        {/* <hr className="ban_line" /> */}
                       </td>
                     );
-                  } else if (cell.value === false) {
+                  } else if (
+                    cell.column.Header === "Ban/Delete" &&
+                    status === false
+                  ) {
                     return (
                       <td>
-                        <h4>Normal</h4>
-                        {/* <hr className="normal_line" /> */}
+                        <Button
+                          variant="contained"
+                          style={{
+                            padding: "0px",
+                            fontSize: "11px",
+                            marginRight: "8px",
+                            marginBottom: "0px",
+                          }}
+                          disabled
+                        >
+                          Unban
+                        </Button>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          style={{
+                            padding: "0px",
+                            fontSize: "11px",
+                            backgroundColor: "#C53030",
+                            width: "5px",
+                            marginBottom: "0px",
+                          }}
+                          onClick={() => handleBan(row.original)}
+                        >
+                          Ban
+                        </Button>
+                        <IconButton
+                          aria-label="delete"
+                          style={{
+                            height: "40px",
+                            width: "40px",
+                          }}
+                        >
+                          <CancelIcon
+                            onClick={() => deleteUser(row.original)}
+                          />
+                        </IconButton>
+                        {/* <hr className="ban_line" /> */}
                       </td>
                     );
                   } else {
-                    return (
-                      <td {...cell.getCellProps()}>
-                        {cell.render("Cell")}
-                        {/* <hr className="normal_line" /> */}
-                      </td>
-                    );
+                    if (cell.value === true) {
+                      return (
+                        <td
+                          style={{
+                            paddingTop: "20px",
+                          }}
+                        >
+                          <h4>Banned</h4>
+                          {/* <hr className="normal_line" /> */}
+                        </td>
+                      );
+                    } else if (cell.value === false) {
+                      return (
+                        <td
+                          style={{
+                            paddingTop: "20px",
+                          }}
+                        >
+                          <h4>Normal</h4>
+                          {/* <hr className="normal_line" /> */}
+                        </td>
+                      );
+                    } else {
+                      return (
+                        <td
+                          {...cell.getCellProps()}
+                          style={{
+                            paddingTop: "20px",
+                          }}
+                        >
+                          {cell.render("Cell")}
+                          {/* <hr className="normal_line" /> */}
+                        </td>
+                      );
+                    }
                   }
-                }
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div
+        className={"pagination"}
+        style={{
+          fontSize: "20px",
+          fontWeight: "bold",
+          display: "tableCell",
+          verticalAlign: "middle",
+        }}
+      >
+        <span
+          style={{
+            fontSize: "18px",
+            fontWeight: "bold",
+          }}
+        >
+          Page {pageIndex + 1} of {pageOptions.length}{" "}
+        </span>
+        <span>
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value ? e.target.value - 1 : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{
+              width: "50px",
+              height: "20px",
+              marginLeft: "5px",
+            }}
+          />
+        </span>
+        <Button
+          style={{ marginLeft: "10px", height: "20px" }}
+          variant="contained"
+          size="small"
+          onClick={() => gotoPage(0)}
+          disabled={!canPreviousPage}
+        >
+          {" "}
+          {"<<"}
+        </Button>
+
+        <Button
+          style={{ marginLeft: "10px", height: "20px" }}
+          variant="contained"
+          size="small"
+          onClick={() => previousPage()}
+          disabled={!canPreviousPage}
+        >
+          Previous
+        </Button>
+        <Button
+          style={{ marginLeft: "10px", height: "20px" }}
+          variant="contained"
+          size="small"
+          onClick={() => nextPage()}
+          disabled={!canNextPage}
+        >
+          Next
+        </Button>
+        <Button
+          style={{ marginLeft: "10px", height: "20px" }}
+          variant="contained"
+          size="small"
+          onClick={() => gotoPage(pageCount - 1)}
+          disabled={!canNextPage}
+        >
+          {" "}
+          {">>"}
+        </Button>
+      </div>
+    </>
   );
 };
