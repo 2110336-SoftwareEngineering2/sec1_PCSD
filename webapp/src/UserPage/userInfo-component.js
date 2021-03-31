731885e84364bff071f3cc74d6e5951c62f2a05d
Perth <perth.ploy@gmail.com>
2021-03-31 21:14:54 +0700

    fix user info page

--- webapp/src/UserPage/userInfo-component.js
+++ webapp/src/UserPage/userInfo-component.js
@@ -2,28 +2,12 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function UserInfo(props) {
-    const [user, setUser] = useState({
-        username: "",
-        email: "",
-        firstname: "",
-        lastname: "",
-        mobileNumber: "",
-        gender: ""
-    });
-    const [info, setInfo] = useState({
-        type: [],
-        pet_type: [],
-        available_day: [],
-        rate: null,
-        city: "",
-        province: "",
-        country: "",
-        description: "",
-    })
+    const [user, setUser] = useState(null);
+    const [info, setInfo] = useState(null)
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`http://localhost:4000/user/${props.username}`)
+        axios.get(`http://localhost:4000/user/profile/${props.username}`)
         .then((res) => {
             const tmp = (({
                 username,
@@ -43,35 +27,33 @@ function UserInfo(props) {
     }, []);
 
     useEffect(() => {
-        axios.post(`http://localhost:4000/user/caretaker/find`, {caretaker: user.email})
-        .then((res) => {
-            const tmp = (({
-                type,
-                pet_type,
-                available_day,
-                rate,
-                city,
-                province,
-                country,
-                description
-              }) => ({ type,pet_type,available_day,rate,city,province,country,description }))(
-                res.data
-              );
-            setInfo(tmp);
-            // setLoading(false);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+        if(user) {
+            axios.post(`http://localhost:4000/user/caretaker/find`, {caretaker: user.email})
+            .then((res) => {
+                const tmp = (({
+                    type,
+                    pet_type,
+                    available_day,
+                    rate,
+                    city,
+                    province,
+                    country,
+                    description
+                  }) => ({ type,pet_type,available_day,rate,city,province,country,description }))(
+                    res.data
+                  );
+                setInfo(tmp);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        }
     }, [user]);
-
-    useEffect(() => {
-        setLoading(false)
-    }, [info])
 
     return(
         <div>
-            { loading ? null : <p>{info.pet_type.join(', ')}</p> }
+            { loading ? <p>Loading...</p> : <p>{info.pet_type.join(', ')}</p> }
         </div>
     )
 }
