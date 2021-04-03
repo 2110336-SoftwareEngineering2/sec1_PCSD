228f6da5e688ecb3c0d28376f4a07cfcc84f7731
Perth <perth.ploy@gmail.com>
2021-04-03 12:45:49 +0700

    display a correct message for rating

--- webapp/src/SearchPage/Results/Result/Result.js
+++ webapp/src/SearchPage/Results/Result/Result.js
@@ -7,9 +7,18 @@ import useStyles from "./styles";
 const Result = (props) => {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+
+  const onClick = () => {
+    console.log(props.userInfo);
+  };
+
   return (
     <div>
-      <Card className={classes.root}>
+      <Card
+        className={classes.root}
+        onClick={onClick}
+        style={{ cursor: "pointer" }}
+      >
         <div className={classes.profile}>
           <img
             className={classes.media}
@@ -21,13 +30,22 @@ const Result = (props) => {
         <div className={classes.info}>
           <Typography className={classes.nameSection} variant="h4">
             {/* 1. */}
-            <span className={classes.name}>{[props.userInfo.user.firstname,props.userInfo.user.lastname].join(' ')}</span>
+            <span className={classes.name}>
+              {[
+                props.userInfo.user.firstname,
+                props.userInfo.user.lastname,
+              ].join(" ")}
+            </span>
           </Typography>
           <Typography className={classes.description} variant="h5">
             {props.userInfo.caretaker.description}
           </Typography>
           <Typography className={classes.address} variant="h6">
-            {[props.userInfo.caretaker.province,props.userInfo.caretaker.city,props.userInfo.caretaker.country].join(", ")}
+            {[
+              props.userInfo.caretaker.province,
+              props.userInfo.caretaker.city,
+              props.userInfo.caretaker.country,
+            ].join(", ")}
           </Typography>
           <Rating
             className={classes.star}
@@ -36,9 +54,16 @@ const Result = (props) => {
             precision={0.5}
             readOnly
           />
-          <Typography className={classes.rating} variant="h6">
-            Review {props.userInfo.caretaker.rate_point.sum_rate.$numberDecimal}/5 ({props.userInfo.caretaker.rate_point.rate_count}){" "}
-          </Typography>
+          {props.userInfo.rate_point_av >= 0 ? (
+            <Typography className={classes.rating} variant="h6">
+              Review {props.userInfo.rate_point_av}/5 (
+              {props.userInfo.caretaker.rate_point.rate_count}){" "}
+            </Typography>
+          ) : (
+            <Typography className={classes.rating} variant="h6">
+              No rating{" "}
+            </Typography>
+          )}
         </div>
         <div className={classes.priceSection}>
           <h6 className={classes.h6}>from</h6>
