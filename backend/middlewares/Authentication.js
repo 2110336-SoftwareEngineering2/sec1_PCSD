a0a3c6934aa3f72f5d0be5c4cce9b140d0dad165
siwakorn21 <siwakornboonmun@gmail.com>
2021-04-05 16:40:18 +0700

    update chat module

--- backend/middlewares/Authentication.js
+++ backend/middlewares/Authentication.js
@@ -6,8 +6,10 @@ module.exports = {
         const user = authToken(req, res);
         if (user.nullToken) {
             res.status(403).send("Forbidden Authentication error. Please set Bearer headers with access token");
+            res.end()
         } else if (user.tokenError) {
             res.status(403).send("Forbidden. The accessToken is invalid.")
+            res.end()
         } else {
             next();
         }
