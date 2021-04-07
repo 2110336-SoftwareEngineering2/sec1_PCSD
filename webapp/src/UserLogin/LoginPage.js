5eb56939fd2e0b8fbfac6679155e1c10d17774bd
UmimChoi <umimchoi@gmail.com>
2021-04-08 02:16:00 +0700

    fixed css checkbox services info

--- webapp/src/UserLogin/LoginPage.js
+++ webapp/src/UserLogin/LoginPage.js
@@ -1,18 +1,34 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
 
+import LoginForm from "./LoginForm";
 import "./LoginPage.css";
 import mainLogo from "../petidcare.png";
 
 function LoginPage() {
+  const logo = (
+    <Col sm>
+      <img src={mainLogo} />
+      <p>online matching platform for caretakers and pet owners</p>
+    </Col>
+  );
+  const tabs = (
+    <Col sm>
+      <Tabs defaultActiveKey="login">
+        <Tab eventKey="login" title="Login">
+          <LoginForm />
+        </Tab>
+        <Tab eventKey="profile" title="Register">
+          Hello 2
+        </Tab>
+      </Tabs>
+    </Col>
+  );
   return (
     <Container id="main-login-page">
       <Row className="mx-5">
-        <Col>
-          <img src={mainLogo} />
-          <p>online matching platform for caretakers and pet owners</p>
-        </Col>
-        <Col>Hi</Col>
+        {logo}
+        {tabs}
       </Row>
     </Container>
   );
