0a755eb883b36819c7d84fb4313ce051eee83daf
UmimChoi <umimchoi@gmail.com>
2021-04-08 04:22:00 +0700

    Update CSS

--- webapp/src/Header/header.js
+++ webapp/src/Header/header.js
@@ -41,7 +41,7 @@ function Header() {
   }, []);
 
   return (
-    <div className="header">
+    <div className="header" id="page_header">
       <div className="header__left">
         <div
           className="header__logo"
